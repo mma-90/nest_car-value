f1aecff098d80e5bde353a445e99da82ee2bebaf
Mohamed Magdy <moh.mag.ali@gmail.com>
2022-11-28 11:39:21 +0200

    simple query builder

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -20,11 +20,20 @@ export class ReportsService {
   }
 
   async getEstimate(getEstimateDto: GetEstimateDto, user: User) {
+    const { make, year, lat, lng, millage, model } = getEstimateDto;
     return await this.repo
       .createQueryBuilder('report')
-      .select('*')
-      // .where('report_make = :make', { make: getEstimateDto.make })
-      .getRawMany();
+      .select('AVG(report.price)', 'avg_price')
+      // .select('id, price, lat')
+      .where('report.make = :make', { make })
+      .andWhere('status is true')
+      .andWhere('report.model = :model', { model })
+      .andWhere('report.year = :year', { year })
+      .andWhere('report.lat BETWEEN :lat-10 AND :lat+10', { lat }) //within +- 10
+      .orderBy('millage', 'DESC')
+      .limit(3)
+      // .getRawMany();
+      .getRawOne();
   }
 
   async changeApproval(id: number, status: boolean, user: User) {
