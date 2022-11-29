7bd53509fc469454285b408b27a1fe228705241e
Mohamed Magdy <moh.mag.ali@gmail.com>
2022-11-29 10:03:25 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // test
   // disabled because it injected directly in app module
   // app.use(
   //   cookieSession({
